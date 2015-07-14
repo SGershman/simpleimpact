64438019c30965a09ace83d50045cad3342be0f7
SGershman <stephen.gershman@gmail.com>
2015-07-14 10:00:23 -0400

    Adding core SWOT Analysis tables

---
+++ lookup.SWOT_Strategic_Priority_Area.Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [lookup].[SWOT_Strategic_Priority_Area](
+	[Strategic_Priority_Area_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Strategic_Priority_Area_Name] [varchar](max) NOT NULL,
+	[Strategic_Priority_Area_Type_ID] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
