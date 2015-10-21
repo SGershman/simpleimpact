5d8f82b2f3276a1bd1cc6dc89bcb630efb23b8ff
SGershman <stephen.gershman@gmail.com>
2015-10-21 14:29:06 -0400

    Fixing naming conventions for lookup tables

---
+++ Tables_lookup/lookup.Role_Committee.Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [lookup].[Role_Committee](
+	[Role_Committee_ID] INT IDENTITY(1,1) NOT NULL,
+	[Role_Committee_Name] [nvarchar](255) NOT NULL,
+	[Role_Committee_Description] [nvarchar](255) NULL
+
+) ON [PRIMARY]
+
+GO
+
+
---
+++ Tables_lookup/lookup.Skill.Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [lookup].[Skill](
+	[Skill_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Skill_Name] [varchar](255) NOT NULL,
+	[Skill_Description] [varchar](255) NULL,
+	[Insert_Timestamp] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+
