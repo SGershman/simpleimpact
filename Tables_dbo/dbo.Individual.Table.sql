fe348f34ec5b5b545543e52b68902cd5e43be677
SGershman <stephen.gershman@gmail.com>
2015-10-21 14:22:30 -0400

    Added individual skills and updated role configuration

--- Tables_dbo/dbo.Individual.Table.sql
+++ Tables_dbo/dbo.Individual.Table.sql
@@ -1,13 +1,14 @@
 CREATE TABLE [dbo].[Individual](
-	[Org_ID] [int] NULL,
+	[Org_ID] [int] NOT NULL,
 	[Org_Chapter_ID] [int] NULL,
-	[Individual_ID] [int] IDENTITY(1,1),
-	[First_Name] [nvarchar](255) NULL,
-	[Last_Name] [nvarchar](255) NULL,
-	[Individual_Role_ID] [int] NULL,
+	[Individual_ID] [int] IDENTITY(1,1) NOT NULL,
+	[First_Name] [nvarchar](255) NOT NULL,
+	[Last_Name] [nvarchar](255) NOT NULL,
 	[City] [nvarchar](255) NULL,
 	[State] [nvarchar](50) NULL,
 	[Zip] [int] NULL,
+	[Phone_1] [varchar](255) NULL,
+	[Phone_2] [varchar](255) NULL,
 	[Race] [nvarchar](50) NULL,
 	[Ethnicity] [nvarchar](50) NULL,
 	[Gender] [nvarchar](50) NULL,
@@ -17,9 +18,9 @@ CREATE TABLE [dbo].[Individual](
 	[Income] [nvarchar](250) NULL,
 	[Industry] [nvarchar](250) NULL,
 	[Financial Position] [nvarchar](250) NULL,
-	[Created_Timestamp] [datetime] NULL,
-	[Entry_Timestamp] [datetime] NULL,
-	[Updated_Timestamp] [datetime] NULL
+	[Created_Timestamp] [datetime] NOT NULL,
+	[Entry_Timestamp] [datetime] NOT NULL,
+	[Updated_Timestamp] [datetime] NOT NULL
 ) ON [PRIMARY]
 
 GO
---
+++ Tables_dbo/dbo.Individual_Skills.Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[Individual_Skills](
+	[Org_ID] [int] NOT NULL,
+	[Org_Chapter_ID] [int] NULL,
+	[Individual_ID] [int] NOT NULL,
+	[Skills_ID] [int] NOT NULL,
+	[Created_Timestamp] [datetime] NOT NULL,
+	[Updated_Timestamp] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+
